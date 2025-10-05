bc9228dbb0fc1c153df25af1f5eae71adfc3a234
KurouSaShi <122357954+KurouSaShi@users.noreply.github.com>
2025-10-05 23:19:38 +0900

    Update config.js

--- blog/config.js
+++ blog/config.js
@@ -4,45 +4,41 @@ const blogConfig = {
     tags: {
         "A:仕事": [
             { title: "A01:大会を開くということは", file: "work/A01.html" },
-            { title: "記事2のタイトル", file: "page2.html" },
-            { title: "記事3のタイトル", file: "page3.html" }
         ],
         "B:ゲーム性": [
-            { title: "記事11のタイトル", file: "page11.html" },
-            { title: "記事12のタイトル", file: "page12.html" }
         ],
         "C:文章": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "D:画像": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "E:配信": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "F:チーム運営": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "G:外部とのやり取り": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "H:選手": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "I:観客": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "J:スケジュール": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "K:ルール": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "L:実例" : [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ],
         "M:小噺": [
-            { title: "記事20のタイトル", file: "page20.html" }
+            
         ]    
     }
 };
