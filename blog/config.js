92544a2673133a6b539d1ab287994b9480cd9c0f
KurouSaShi <122357954+KurouSaShi@users.noreply.github.com>
2025-10-05 18:51:26 +0900

    Create config.js

---
+++ blog/config.js
@@ -0,0 +1,18 @@
+// ブログの設定
+const blogConfig = {
+    title: "My Blog",
+    tags: {
+        "tag1": [
+            { title: "記事1のタイトル", file: "page1.html" },
+            { title: "記事2のタイトル", file: "page2.html" },
+            { title: "記事3のタイトル", file: "page3.html" }
+        ],
+        "tag2": [
+            { title: "記事11のタイトル", file: "page11.html" },
+            { title: "記事12のタイトル", file: "page12.html" }
+        ],
+        "tag3": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ]
+    }
+};
