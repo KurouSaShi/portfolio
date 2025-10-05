9a35e1b48b29959990557d55ee8c086faff916b7
KurouSaShi <122357954+KurouSaShi@users.noreply.github.com>
2025-10-05 22:53:47 +0900

    Update config.js

--- blog/config.js
+++ blog/config.js
@@ -2,17 +2,48 @@
 const blogConfig = {
     title: "音ゲー大会Blog",
     tags: {
-        "tag1": [
-            { title: "記事1のタイトル", file: "page1.html" },
+        "A:仕事": [
+            { title: "A01:大会を開くということは", file: "work/A01.html" },
             { title: "記事2のタイトル", file: "page2.html" },
             { title: "記事3のタイトル", file: "page3.html" }
         ],
-        "tag2": [
+        "B:ゲーム性": [
             { title: "記事11のタイトル", file: "page11.html" },
             { title: "記事12のタイトル", file: "page12.html" }
         ],
-        "tag3": [
+        "C:文章": [
             { title: "記事20のタイトル", file: "page20.html" }
-        ]
+        ],
+        "D:画像": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "E:配信": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "F:チーム運営": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "G:外部とのやり取り": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "H:選手": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "I:観客": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "J:スケジュール": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "K:ルール": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "L:実例: [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        "M:小噺": [
+            { title: "記事20のタイトル", file: "page20.html" }
+        ],
+        
     }
 };
